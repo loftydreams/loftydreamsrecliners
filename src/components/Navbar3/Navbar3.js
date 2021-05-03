087d0533be924ce3916deab626c45acbc1d6674b
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-05-03 23:21:51 +0530

    adding search functionality

--- src/components/Navbar3/Navbar3.js
+++ src/components/Navbar3/Navbar3.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import "./Navbar3.css";
-import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
-import { Link } from 'react-router-dom';
-const Navbar3 = () => {
-    return (
-        <>
-        <div className="navbar3-body">
-   <Link to ="/single-seater">        <span> Single Seater Recliners </span> </Link>
-    <Link to ="/living-room">      <span> Living Room Recliners </span> </Link>  
-      <Link to="/home-theater"  >    <span> Home Theater Recliners </span> </Link>
-       <Link to="/lounger" >  <span> Lounger Sofa </span>  </Link>
-        <Link to ="/riser" >   <span> Riser Recliners </span> </Link> 
-         
-            
-        </div>
-        <div className="search-bar-bottom">
-                <input type="text" placeholder="Search..."  className="search-input-bottom" />
-              
-          
-            <SearchOutlinedIcon className="search-icon-bottom" />
-            </div>
-        <hr className="hr-bottom"></hr>
-        </>
-    )
-}
+import { Link } from "react-router-dom";
+import SearchBar from "../search-products/search-bar";
 
-export default Navbar3
+import "./Navbar3.css";
+
+const Navbar3 = () => {
+  return (
+    <>
+      <div className="navbar3-body">
+        <Link to="/single-seater">
+          <span> Single Seater Recliners </span>
+        </Link>
+        <Link to="/living-room">
+          <span> Living Room Recliners </span>
+        </Link>
+        <Link to="/home-theater">
+          <span> Home Theater Recliners </span>
+        </Link>
+        <Link to="/lounger">
+          <span> Lounger Sofa </span>
+        </Link>
+        <Link to="/riser">
+          <span> Riser Recliners </span>
+        </Link>
+      </div>
+      <SearchBar cname="search-bar-bottom" />
+      <hr className="hr-bottom"></hr>
+    </>
+  );
+};
+
+export default Navbar3;
