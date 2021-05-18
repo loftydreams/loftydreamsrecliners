1321018be062acc003ca1af1e06fde46d608009d
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-05-18 22:44:27 +0530

    improving seo and adding analytics

--- src/firebase.js
+++ src/firebase.js
@@ -16,6 +16,7 @@ firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+export const analytics = firebase.analytics();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
