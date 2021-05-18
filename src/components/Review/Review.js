d57071b7a549f914259c3c25cec6ab0d72959443
deepakmalik264 <deepakmalik264@gmail.com>
2021-05-18 23:35:23 +0530

    changes Review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -14,7 +14,10 @@ export const Review = () => {
             <div className="box-top">
               <div className="profile">
                 <div className="profile-img">
-                  <img src="Images/Icons8-customer-480.png"  alt ="review-person" />
+                  <img
+                    src="Images/Icons8-customer-480.png"
+                    alt="review-person"
+                  />
                 </div>
 
                 <div className="name-user">
@@ -40,7 +43,10 @@ export const Review = () => {
             <div className="box-top">
               <div className="profile">
                 <div className="profile-img">
-                  <img src="Images/Icons8-customer-480.png"  alt="review-person" />
+                  <img
+                    src="Images/Icons8-customer-480.png"
+                    alt="review-person"
+                  />
                 </div>
 
                 <div className="name-user">
@@ -53,11 +59,10 @@ export const Review = () => {
 
             <div className="client-comment">
               <p>
-                I have purchase powered recliner for over a month and I give
-                them 5 stars. The price is certainly worth it. This recliner is
-                comfortable. I can get up from it comfortably. My old recliner
-                was a rocker recliner and I struggled to get up. This new
-                recliner seat is a little higher which helps me to get up.
+                I Purchased Manual LDRS-01 Nestro. I could not believe how nice
+                it was. I have other recliner that I paid 2 times the price for
+                and I feel this one is just as good. The back is tall that is
+                important for me as I have back problem.
               </p>
             </div>
           </div>
