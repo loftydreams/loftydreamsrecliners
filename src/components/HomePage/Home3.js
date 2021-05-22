5029b65908cb738c6645d14f824110c9477dc58e
Ashutosh Tripathi <ashutoshtripathi@Ashutoshs-MacBook-Air.local>
2021-05-22 18:10:22 +0530

    seller.svg added

--- src/components/HomePage/Home3.js
+++ src/components/HomePage/Home3.js
@@ -5,7 +5,7 @@ const Home3 = () => {
         <div className="home3-body">
 
               
-            <img src="Images/seller.svg" alt="seller" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/loftydreams-307f2.appspot.com/o/React-Images%2Fseller.svg?alt=media&token=f2b9d8f9-f8a1-4bcc-b833-c8ed05d0dc9f" alt="seller" />
             {/* <div className="home3-heading">
                 <h1> WE ARE THE TRUSTED SELLER AT</h1>
             </div>
