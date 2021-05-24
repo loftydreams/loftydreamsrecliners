7fb1fd7ae01d6030934238444e5629d8c41af4d1
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-05-24 13:23:53 +0530

    updating icons

--- src/components/Navbar1/Feedback.js
+++ src/components/Navbar1/Feedback.js
@@ -3,6 +3,7 @@ import { Form, Button } from "react-bootstrap";
 import { makeStyles } from "@material-ui/core/styles";
 import Rating from "@material-ui/lab/Rating";
 import Box from "@material-ui/core/Box";
+import DialogBox from "./DialogBox";
 
 import { firestore } from "../../firebase";
 
@@ -31,12 +32,21 @@ const useStyles = makeStyles({
 
 const Feedback = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
   const [value, setValue] = useState(4);
   const [hover, setHover] = useState(-1);
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [feedback, setFeedback] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -51,84 +61,87 @@ const Feedback = () => {
           rating: value,
         })
         .then((data) => {
-          console.log(data);
           setValue(4);
           setName("");
           setPhone("");
           setEmail("");
           setFeedback("");
+          handleClickOpen();
         });
     }
   };
 
   return (
-    <div className="feedback-wrapper">
-      <h2>Feedback Form </h2>
+    <>
+      <div className="feedback-wrapper">
+        <h2>Feedback Form </h2>
 
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control
-            type="name"
-            placeholder="Name"
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control
-            type="phone"
-            placeholder="Contact"
-            required
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <span> Rating</span> <br></br>
-        <div className={classes.root}>
-          <Rating
-            name="hover-feedback"
-            required
-            value={value}
-            precision={0.5}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-            onChangeActive={(event, newHover) => {
-              setHover(newHover);
-            }}
-          />
-          {value !== null && (
-            <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>
-          )}
-        </div>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Control
-            as="textarea"
-            required
-            rows={5}
-            placeholder="Your Valuable Feedback"
-            value={feedback}
-            onChange={(e) => setFeedback(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control
+              type="name"
+              placeholder="Name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control
+              type="phone"
+              placeholder="Contact"
+              required
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <span> Rating</span> <br></br>
+          <div className={classes.root}>
+            <Rating
+              name="hover-feedback"
+              required
+              value={value}
+              precision={0.5}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              onChangeActive={(event, newHover) => {
+                setHover(newHover);
+              }}
+            />
+            {value !== null && (
+              <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>
+            )}
+          </div>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Control
+              as="textarea"
+              required
+              rows={5}
+              placeholder="Your Valuable Feedback"
+              value={feedback}
+              onChange={(e) => setFeedback(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+      <DialogBox open={open} handleClose={handleClose} />
+    </>
   );
 };
 
